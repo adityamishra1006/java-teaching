3550de4655a1b277e861cc75592f81a387c9dc3c
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-02-10 20:07:03 +0530

    Create Day1.java

---
+++ Day1.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("Aditya Mishra");
+        System.out.println("Ayush Singh");
+        System.out.println("5+5");
+        String jasjkxak = "Aditya ";
+        System.out.println(jasjkxak);
+
+        // We can also perform operations inside println method
+        System.out.println(5+5);
+        System.out.println(5*5);
+        System.out.println(5/5);
+        System.out.println(7-5);
+    }
+}
