5f884b62d031880204229511881217520c88bcdf
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-02-11 20:16:29 +0530

    Create Day2Part1.java

---
+++ Day2Part1.java
@@ -0,0 +1,15 @@
+public class Add {
+    public static void main(String[] args) {
+        String name = "Ayush Singh";
+        System.out.println("Name : " + name);
+
+        boolean ayushTaklsNahiHai = true;
+        System.out.println("Ayush Ke Sar me Bal Hai : " + ayushTaklsNahiHai);
+
+        int age = 19;
+        // System.out.println(age);
+        age = 20;
+        System.out.println("Ayush ka age : " + age);
+
+    }    
+}
