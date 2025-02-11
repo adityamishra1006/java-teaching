3834302bb7af07fa4cf2beada74096a18ef3e07c
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-02-11 20:42:35 +0530

    Create Day2Part2.java

---
+++ Day2Part2.java
@@ -0,0 +1,17 @@
+public class Day2Part2 {
+    
+    public static void main(String[] args) {
+        String name = "Ayush Singh";
+        int age = 19;
+        double weight = 56.76;
+        float height = (float) 5.4;
+        boolean ayushLearningJAVA = true;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : "+age);
+        System.out.println("Weight : "+weight);
+        System.out.println("Height : "+height);
+        System.out.println("Ayush JAVA sekh raha hai : " +ayushLearningJAVA);
+
+    }
+}
