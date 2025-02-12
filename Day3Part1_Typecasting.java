ca0e8cbda652438ad41726191928c60ced068348
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-02-12 19:55:54 +0530

    Create Day3Part1_Typecasting.java

---
+++ Day3Part1_Typecasting.java
@@ -0,0 +1,15 @@
+public class Day3Part1 {
+    public static void main(String[] args) {
+        // Automatic Typecasting
+        int a = 10;
+        double b = a;
+        System.out.println(a);
+        System.out.println(b);
+
+        // Explicit Typecasting
+        double d = 34.56;
+        System.out.println(d);
+        int e = (int) d;
+        System.out.println(e);
+    }
+}
