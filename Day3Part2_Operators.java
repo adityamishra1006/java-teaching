40bcb3b5106e7085ca2e403b7ed1900219f6cdaa
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-02-12 20:15:08 +0530

    Create Day3Part2_Operators.java

---
+++ Day3Part2_Operators.java
@@ -0,0 +1,27 @@
+public class Day3Part2_Operators {
+    public static void main(String[] args) {
+        // Arithmatic Operators
+        int number1 = 5;
+        int number2 = 6;
+
+        // Addition
+        int addition = number1 + number2;
+        System.out.println(addition);
+
+        // Subtraction
+        int subtraction = number2 - number1;
+        System.out.println(addition);
+        
+        // Multiplication
+        int multiplication = number1*number2;
+        System.out.println(multiplication);
+
+        // Division
+        double division = (double) number1/number2;
+        System.out.println(division);
+
+        // Modulo
+        int modulo = number2 % number1;
+        System.out.println(modulo);
+    }
+}
