d111b1d01c6703d14926efaa7847aa22d1ccba32
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-02-13 19:52:18 +0530

    Create GreatestAmongThreeNumber.java

---
+++ GreatestAmongThreeNumber.java
@@ -0,0 +1,15 @@
+public class GreatestAmongThreeNumber {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 6;
+
+        if(a < b){
+            System.out.println(a);
+        }else if(b < c){
+            System.out.println(b);
+        }else{
+            System.out.println(c);
+        }
+    }
+}
