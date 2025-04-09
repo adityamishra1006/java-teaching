e6c4d76e124d0c830429b9b22e6561b431b30b8b
Aditya Mishra <96217543+adityamishra1006@users.noreply.github.com>
2025-04-09 20:24:01 +0530

    Create Print_Number_From_One_to_Ten.java

---
+++ Print_Number_From_One_to_Ten.java
@@ -0,0 +1,7 @@
+public class Print_Number_From_One_to_Ten {
+    public static void main(String[] args){
+        for(int i = 1; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
